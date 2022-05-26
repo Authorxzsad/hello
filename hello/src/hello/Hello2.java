05aabb964b2bedddb39604d2e8a76ce20edeaea8
Authorxzsad <bong890@naver.com>
2022-05-26 14:07:25 +0900

    hello

--- hello/src/hello/Hello2.java
+++ hello/src/hello/Hello2.java
@@ -3,10 +3,6 @@ package hello;
 public class Hello2 {
 	
   public static void main(String[] args) {
-<<<<<<< HEAD
-		 System.out.println("Hi33");
-=======
-		 System.out.println("Hi222");
->>>>>>> branch 'master' of https://github.com/Authorxzsad/hello.git
+		 System.out.println("Hello2");
   }
 }
